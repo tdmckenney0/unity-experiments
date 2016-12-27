1c51af4333c7ae81943b2aff16c3729073f3b447
tmckenney7 <tmckenney7@outlook.com>
2016-12-26 23:42:55 -0500

    Initial Test Rigging
    Initial Test Rigging

---
+++ Assets/Scripts/BladeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BladeController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed = 1;
+    public float accel = 1; 
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float throttle = 0.0f;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            throttle += accel; 
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            throttle -= accel;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, throttle);
+
+        rb.AddForce(movement * speed);
+    }
+}
